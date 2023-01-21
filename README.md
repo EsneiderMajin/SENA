ba8ba5f2cad661ad59402f676831af19e44a08a1
ArnoldCatamuscay <ascatamuscay@unicauca.edu.co>
2023-01-21 14:30:01 -0500

    iconos añadidos

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
  Si se modifica el archivo models.py ejecutar los siguientes 2 comandos para cargar los cambios en la BD:  
     python manage.py makemigrations  
     python manage.py migrate  
+# Links
+ Link para íconos: https://fontawesome.com/v6/search?o=r&m=free
--- SENA/settings.py
+++ SENA/settings.py
@@ -119,7 +119,7 @@ LOGOUT_REDIRECT_URL = reverse_lazy('login')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Aplicaciones/Academico/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
